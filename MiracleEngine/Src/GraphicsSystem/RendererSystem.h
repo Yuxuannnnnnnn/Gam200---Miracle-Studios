7c916c227ff4b7f1f62fc1fd7f729859533be314
satsey <54670828+satsey@users.noreply.github.com>
2019-09-12 17:29:05 +0800

    linedraw test

--- MiracleEngine/Src/GraphicsSystem/RendererSystem.h
+++ MiracleEngine/Src/GraphicsSystem/RendererSystem.h
@@ -129,6 +129,45 @@ public:
 	}
 	void DrawLine(int x1, int y1, int x2, int y2)
 	{
+		int windowWidth = 800;
+		int windowHeight = 600;
+
+		glm::mat4 proj = glm::ortho(-(float)windowWidth / 2, (float)windowWidth / 2, -(float)windowHeight / 2, (float)windowHeight / 2);
+
+		std::string vertexShader =
+			"#version 330 core\n"
+			"uniform mat4 u_MVP;\n"
+			"layout(location = 0) in vec4 position;"
+			"\n"
+			"void main()\n"
+			"{\n"
+			"gl_Position = u_MVP * position;\n"
+			"}\n";
+
+		std::string fragmentShader =
+			"#version 330 core\n"
+			"layout(location = 0) out vec4 color;"
+			"uniform vec4 u_Color;\n"
+			"void main()\n"
+			"{\n"
+			"color = u_Color;\n"
+			"}\n";
+
+		GLuint shader = CreateShader(vertexShader, fragmentShader);
+		glUseProgram(shader);
+
+
+		int x = (x1 + x2) / 2;
+		int y = (y1 + y2) / 2;
+
+		glm::mat4 model = glm::translate(glm::mat4(1.0f), glm::vec3(0, 0, 0));
+		glm::mat4 mvp = proj * model;
+
+		int location = glGetUniformLocation(shader, "u_Color");
+		glUniform4f(location, 1.0f, 0.0f, 0.0f, 1.0f);
+
+		location = glGetUniformLocation(shader, "u_MVP");
+		glUniformMatrix4fv(location, 1, GL_FALSE, &mvp[0][0]);
 
 		GLfloat lineVertices[] =
 		{
@@ -136,57 +175,19 @@ public:
 			100,300,0
 		};
 
+		VertexBuffer vbo(lineVertices, 3 * 2 * sizeof(GLfloat));
+		glEnableVertexAttribArray(0);
 
-
-		glVertexPointer(3, GL_FLOAT, 0, lineVertices);
-		glDrawArrays(GL_LINES, 0, 2);
+		glVertexAttribPointer(0,	
+			3,
+			GL_FLOAT, // they a float
+			GL_FALSE, // don't need to be normalised
+			0,  // how many floats to the next one(be aware btVector3 uses 4 floats)
+			0  // where do they start as an index); 
+		);
+		glEnableVertexAttribArray(0);
+		glDrawArrays(GL_LINES, 0, 6);
 		
-
-		////_meshmanager.Update()
-
-		//glEnableVertexAttribArray(0);
-
-		//glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, 0);
-
-		//int windowWidth = 800;
-		//int windowHeight = 600;
-
-		//glm::mat4 proj = glm::ortho(-(float)windowWidth / 2, (float)windowWidth / 2, -(float)windowHeight / 2, (float)windowHeight / 2);
-
-		//std::string vertexShader =
-		//	"#version 330 core\n"
-		//	"uniform mat4 u_MVP;\n"
-		//	"layout(location = 0) in vec4 position;"
-		//	"\n"
-		//	"void main()\n"
-		//	"{\n"
-		//	"gl_Position = u_MVP * position;\n"
-		//	"}\n";
-
-		//std::string fragmentShader =
-		//	"#version 330 core\n"
-		//	"layout(location = 0) out vec4 color;"
-		//	"uniform vec4 u_Color;\n"
-		//	"void main()\n"
-		//	"{\n"
-		//	"color = u_Color;\n"
-		//	"}\n";
-
-		//GLuint shader = CreateShader(vertexShader, fragmentShader);
-		//glUseProgram(shader);
-
-
-		//int x = (x1 + x2) / 2;
-		//int y = (y1 + y2) / 2;
-
-		//glm::mat4 model = glm::translate(glm::mat4(1.0f), glm::vec3(x, y, 0));
-		//glm::mat4 mvp = proj * model;
-
-		//int location = glGetUniformLocation(shader, "u_Color");
-		//glUniform4f(location, 1.0f, 0.0f, 0.0f, 1.0f);
-
-		//location = glGetUniformLocation(shader, "u_MVP");
-		//glUniformMatrix4fv(location, 1, GL_FALSE, &mvp[0][0]);
 
 		//// this one encapsulate into another class
 		//glDrawArrays(GL_LINES, 0, 2);
