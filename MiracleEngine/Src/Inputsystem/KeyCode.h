a0de4880ad812e90d371f86cfaf29edee540a0b5
satsey <54670828+satsey@users.noreply.github.com>
2019-09-18 18:32:13 +0800

    InputSystem

---
+++ MiracleEngine/Src/Inputsystem/KeyCode.h
@@ -0,0 +1,16 @@
+#pragma once
+
+enum KeyCode
+{
+	NONE,
+	MOUSE_LBUTTON,
+	MOUSE_RBUTTON,
+	KEY_CANCEL,
+	MOUSE_MBUTTON,
+	X1_mouse_button,
+	X2_mouse_button,
+	unassigned,
+	KEYB_BACKSPACE,
+	KEYB_TAB,
+
+};
