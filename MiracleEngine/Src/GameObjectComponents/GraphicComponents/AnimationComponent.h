a46de09c2c693e801b6c1c7a5e212303802c5935
satsey <54670828+satsey@users.noreply.github.com>
2019-10-31 19:02:44 +0800

    compoents

--- MiracleEngine/Src/GameObjectComponents/GraphicComponents/AnimationComponent.h
+++ MiracleEngine/Src/GameObjectComponents/GraphicComponents/AnimationComponent.h
@@ -1,8 +1,33 @@
 #pragma once
 
-
+#include "../Animation/Animation.h"
+#include <vector>
 
 class AnimationComponent: public IComponentSystem
 {
+public:
+	void AddAnimation(const Animation& animation)
+	{
+		_animation.push_back(new Animation(animation));
+	}
+	void SetCurrentAnim(int curr)
+	{
+		_currentAnimation = curr;
+	}
+	void SetStartFrame(int frame)
+	{
+		_startingFrame = frame;
+	}
+	~AnimationComponent()
+	{
+		for (auto e : _animation)
+		{
+			delete e;
+		}
+	}
+private:
+	std::vector<Animation*> _animation;
+	int _currentAnimation;
+	int _startingFrame;
 };
 
