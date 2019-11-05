094de060030bf228b842002259269d0d85bfa933
bnj292 <brandon.ng.jun.jie@gmail.com>
2019-11-06 04:50:59 +0800

    Add ButtonUI

---
+++ MiracleEngine2/Src/Gameplay/ButtonUI.cpp
@@ -0,0 +1,13 @@
+#include "PrecompiledHeaders.h"
+#include "../Engine/EngineSystems.h"
+#include "../GameObjectComponents/LogicComponents/PrecompiledScriptType.h"
+
+ButtonUI::ButtonUI()
+{
+
+}
+
+void ButtonUI::Update(double dt)
+{
+
+}
