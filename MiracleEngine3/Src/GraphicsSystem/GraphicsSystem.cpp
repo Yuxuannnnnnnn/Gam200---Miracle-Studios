111b4daf583b3407e25b266a5d1f40a687bb5267
satsey <yimin950@gmail.com>
2019-12-04 19:41:29 +0800

    instruction

--- MiracleEngine3/Src/GraphicsSystem/GraphicsSystem.cpp
+++ MiracleEngine3/Src/GraphicsSystem/GraphicsSystem.cpp
@@ -266,6 +266,26 @@ void GraphicsSystem::Update(double dt)
 
 		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
 	}
+	else if(EngineSystems::GetInstance()._sceneManager->GetCurrentScene() == Scenes::INSTRUCTION)
+	{
+		int a = 0;
+		_quadmesh.Select();
+		_textureManager._textureMap["Instruction"]->Select();
+		_shader.Select();
+
+		glm::mat4 translate = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, 15.0f));
+		glm::mat4 rotate = glm::rotate(glm::mat4(1.0f), 0.0f, glm::vec3(0, 0, 1));
+		glm::mat4 model = translate * glm::scale(glm::mat4(1.0f),
+			glm::vec3((float)_windowWidth, (float)_windowHeight, 1.0f));
+
+		glm::mat4 mvp = _proj * _camera.GetCamMatrix() * model;
+
+		//_shader.SetUniform4f("u_Color", 1.0f, 0.0f, 0.0f, 1.0f);
+
+		_shader.SetUniformMat4f("u_MVP", mvp);
+
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
+	}
 	else
 	{
 		_textureManager._textureMap["HP_Bar"]->Select();
