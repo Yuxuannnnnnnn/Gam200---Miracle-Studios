58d78efa35f1fa9efeff2ba511594200cd0e927d
Yuxuannnnnnnn <luoyuxuan43@gmail.com>
2019-12-19 17:16:14 +0800

    Deleted Redundant engines

--- Opengl draft/OpenGLTutorial01/Isystem.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-class ISystem
-{
-public:
-	virtual void Init() = 0;
-	virtual void Update() = 0;
-	virtual void Exit() = 0;
-};
--- Opengl draft/OpenGLTutorial01/graphicsystem/shader.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include "glew.h"
-#include <string>
-
-
