2e4945702f116f02ac1e025bd363991300d3d2a1
satsey <yimin950@gmail.com>
2019-12-22 18:21:14 +0800

    camera

--- MiracleEngine/Src/Engine/SystemGraphics/CameraManager.cpp
+++ MiracleEngine/Src/Engine/SystemGraphics/CameraManager.cpp
@@ -1,2 +1,30 @@
 #include "PrecompiledHeaders.h"
 #include "CameraManager.h"
+
+void CameraManager::Update()
+{
+
+}
+
+glm::mat4 CameraManager::GetMainCamMatrix()
+{
+	for (auto& camComponentpair : EngineSystems::GetInstance()._gameObjectFactory->getCameraComponent())
+	{
+		if (camComponentpair.second->GetParentId() < 1000 || camComponentpair.second->GetParentPtr()->GetDestory())
+			continue;
+
+		size_t objID = camComponentpair.first;	//Get GameObjectID
+		TransformComponent* transformComponent = MyTransformManager.GetTransform(objID); //Get transform from GameObjectID
+
+		if (!transformComponent)
+			continue;
+
+		TransformComponent x = *transformComponent;
+
+		glm::mat4 translate = glm::translate(glm::mat4(1.0f), glm::vec3(x_pos
+			, y_pos, 1));
+		glm::mat4 rotate = glm::rotate(glm::mat4(1.0f), transformComponent->GetRotate(), glm::vec3(0, 0, 1));
+		return translate * rotate * glm::scale(glm::mat4(1.0f),
+			glm::vec3(x_scale, y_scale, 1.0f));
+	}
+}
--- MiracleEngine/Src/Engine/SystemGraphics/CameraManager.h
+++ MiracleEngine/Src/Engine/SystemGraphics/CameraManager.h
@@ -1,8 +1,28 @@
 #pragma once
 
 // a manager class to manage all the camera
+#include <map>
+#include <string>
+#include "Camera.h"
+#include "glm/glm.hpp"
+#include "glm/gtc/matrix_transform.hpp"
 
 class CameraManager
 {
+public:
+	// update the camera component
 
+	void Update();
+	glm::mat4 GetMainCamMatrix();
+
+private:
+	std::unordered_map<std::string, Camera> _cameraList;
+	std::string _currCamera;  // current camera in use
+
+	// to be removed later
+
+	float x_pos = 1.0f;
+	float y_pos = 1.0f;
+	float x_scale = 1.0f;
+	float y_scale = 1.0f;
 };
