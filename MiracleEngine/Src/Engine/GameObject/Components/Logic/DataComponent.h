41cc1d32eb3886fae0a67291b3e52b2651754612
bnj292 <brandon.ng.jun.jie@gmail.com>
2020-01-16 17:20:38 +0800

    Update to allow merge

--- MiracleEngine/Src/Engine/GameObject/Components/Logic/DataComponent.h
+++ MiracleEngine/Src/Engine/GameObject/Components/Logic/DataComponent.h
@@ -26,6 +26,8 @@ public:
 
 	virtual void BindLuaValues(sol::state& lua, std::string& tableName)
 		{ std::cout << "WARNING: DataComponent::BindLuaValues() being called! \n"; }
+	virtual void SaveLuaValues(sol::state& lua, std::string& tableName)
+		{ std::cout << "WARNING: DataComponent::SaveLuaValues() being called! \n"; }
 };
 
 //		changed class DataHealth : public IComponent
@@ -36,20 +38,20 @@ public:
 		// BRANDON ADD THIS SHIT
 
 
-class DataMove : public DataComponent
+class DataMoveComponent : public DataComponent
 {
 	Vector3 _position, _scale;
 	float _rotation;
 public:
-	DataMove();
-	virtual ~DataMove();
+	DataMoveComponent();
+	virtual ~DataMoveComponent();
 	virtual std::string ComponentName() const override;
 	virtual void SerialiseComponent(Serialiser& document) override;
 	virtual void DeSerialiseComponent(DeSerialiser& prototypeDoc) override;
 	virtual void Inspect() override;
 	void DeserialiseComponentSceneFile(IComponent* protoCom, rapidjson::Value& value, rapidjson::MemoryPoolAllocator<>& allocator)		override { return; }
 
-	DataMove* CloneComponent() { return nullptr; }
+	DataMoveComponent* CloneComponent() { return nullptr; }
 
 	virtual void BindLuaValues(sol::state& lua, std::string& tableName) override {
 		TransformComponent* temp = (TransformComponent*)parentLogic->GetSibilingComponent(ComponentId::CT_Transform);
@@ -59,6 +61,12 @@ public:
 		lua[tableName]["POSITION"] = &_position;
 		lua[tableName]["SCALE"] = &_scale;
 		lua[tableName]["ROTATION"] = &_rotation;
+	}
+	virtual void SaveLuaValues(sol::state& lua, std::string& tableName) override {
+		TransformComponent* temp = (TransformComponent*)parentLogic->GetSibilingComponent(ComponentId::CT_Transform);
+		temp->SetPos(lua[tableName]["POSITION"]);
+		temp->SetScale(lua[tableName]["SCALE"]);
+		temp->SetRotate(lua[tableName]["ROTATION"]);
 	}
 };
 
@@ -95,7 +103,7 @@ public:
 	virtual void BindLuaValues(sol::state& lua, std::string& tableName) override {
 		lua[tableName]["DATA"] = _Capacity;
 	}
-	DataAmmo* CloneComponent() { return new DataAmmo(*this); }
+	DataShieldComponent* CloneComponent() { return new DataShieldComponent(*this); }
 };
 
 //class DataAmmo : public DataComponent
