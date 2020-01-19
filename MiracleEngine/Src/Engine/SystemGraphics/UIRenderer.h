ab0e4761e101fddc72dc836c77c901215119db75
satsey <yimin950@gmail.com>
2020-01-19 17:48:15 +0800

    uirenderer

--- MiracleEngine/Src/Engine/SystemGraphics/UIRenderer.h
+++ MiracleEngine/Src/Engine/SystemGraphics/UIRenderer.h
@@ -1,11 +1,13 @@
 #pragma once
 
-
+#include "UIMesh.h"
+#include "../Engine/GameObject/Components/Graphic/UIComponent.h"
+#include <map>
 
 class UIRenderer
 {
 public:
-
+	void Update(std::unordered_map<size_t, IComponent*> uiMap);
 private:
-
+	UIMesh _uiMesh;
 };
