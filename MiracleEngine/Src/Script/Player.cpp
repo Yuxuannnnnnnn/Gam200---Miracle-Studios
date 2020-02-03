fbdea90de3a29c7aa74cfd6f7c5e77cb3bed73bd
boonwee24 <59058149+boonwee24@users.noreply.github.com>
2020-02-03 19:01:44 +0800

    add player death

--- MiracleEngine/Src/Script/Player.cpp
+++ MiracleEngine/Src/Script/Player.cpp
@@ -302,7 +302,7 @@ void Player::UpdateInput()
 	if (EngineSystems::GetInstance()._inputSystem->KeyDown(KeyCode::KEYB_8) ||
 		EngineSystems::GetInstance()._inputSystem->KeyHold(KeyCode::KEYB_8))
 	{
-		((AnimationComponent*)this->GetSibilingComponent(ComponentId::CT_Animation))->SetCurrentAnim("jump");
+		((AnimationComponent*)this->GetSibilingComponent(ComponentId::CT_Animation))->SetCurrentAnim("death1");
 	}
 	
 
