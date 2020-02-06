c9c735f42d3181f4dd6fddb7fc0b3977ee2f1ee0
bnj292 <brandon.ng.jun.jie@gmail.com>
2020-02-06 20:07:28 +0800

    Fixed enemies just walking towards player

--- MiracleEngine/Src/Script/Spawner.cpp
+++ MiracleEngine/Src/Script/Spawner.cpp
@@ -1,38 +1,12 @@
 #include "PrecompiledHeaders.h"
 #include "Script/Spawner.h"
 
-void Spawner::SerialiseComponent(Serialiser& document)
-{
-	if (document.HasMember("Health") && document["Health"].IsInt())
-	{
-		_health = (document["Health"].GetInt());
-	}
-	if (document.HasMember("SpawnType") && document["SpawnType"].IsInt())
-	{
-		_spawntype = (document["SpawnType"].GetInt());
-	}
-	if (document.HasMember("SpawnRate") && document["SpawnRate"].IsDouble())
-	{
-		_timerCooldownSpawn = (document["SpawnRate"].GetDouble());
-	}
-
-}
-void Spawner::DeSerialiseComponent(DeSerialiser& prototypeDoc)
-{
-	rapidjson::Value value;
-
-}
-void Spawner::Inspect()
-{
-
-}
-
 Spawner::Spawner() :
 	_init{ false },
-	_health{ 10 },
-	_spawntype{ 0 },
-	_timerSpawn{ -1.0 },
-	_timerCooldownSpawn{ 20 }
+	_health{ 0 },
+	_spawnType{ 0 },
+	_timerSpawn{ 0 },
+	_timerSpawnCooldown{ 10 }
 {
 }
 
@@ -43,16 +17,14 @@ Spawner* Spawner::Clone()
 
 void Spawner::Init()
 {
-	//Spawn();
+	_init = true;
 }
 
 void Spawner::Update(double dt)
 {
 	if (!_init)
-	{
 		Init();
-		_init = true;
-	}
+
 	_timerSpawn -= dt;
 	if (_timerSpawn <= 0)
 		Spawn();
@@ -61,26 +33,23 @@ void Spawner::Update(double dt)
 void Spawner::Spawn()
 {
 	// reset timer for spawning
-	_timerSpawn = _timerCooldownSpawn;
+	_timerSpawn = _timerSpawnCooldown;
 	
 	// add code for rng spawning if want to
-
-	// spawn enemy based on _spawntype
+	std::srand((unsigned)std::time(0));
+	int random = 1 + std::rand() % 2;
 	GameObject* enemy = nullptr;
-	switch (_spawntype)
+	switch (_spawnType)
 	{
 	case 1:
 		enemy = MyFactory.CloneGameObject(MyResourceSystem.GetPrototypeMap()["Enemy"]);
 		break;
 	case 2:
-		break;
-	case 3:
+		enemy = MyFactory.CloneGameObject(MyResourceSystem.GetPrototypeMap()["EnemyTwo"]);
 		break;
 	default:
 		return;
 	}
-
-	// set enemy to position of the spawner
 	((TransformComponent*)enemy->GetComponent(ComponentId::CT_Transform))->SetPos(
 		((TransformComponent*)(GetSibilingComponent(ComponentId::CT_Transform)))->GetPos());
 }
