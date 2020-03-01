c35ca6995bd476bfc822532a6ba8c920e63296bb
yinshuyu <love0love.ys@gmail.com>
2020-03-01 19:23:56 +0800

    cleaning up

--- MiracleEngine/Src/Engine/GameObject/Components/Graphic/TransformComponent.cpp
+++ MiracleEngine/Src/Engine/GameObject/Components/Graphic/TransformComponent.cpp
@@ -6,38 +6,20 @@ std::string TransformComponent::ComponentName() const
 }
 
 
-//void TransformComponent::SerialiseComponentFromLevelFile(rapidjson::Value& document)
-//{
-//	if (document.HasMember("Position"))	//Checks if the variable exists in .Json file
-//	{
-//		_pos = Vector3{ document["Position"][0].GetFloat(), document["Position"][1].GetFloat(), 1 };
-//	}
-//
-//	if (document.HasMember("Scale"))
-//	{
-//		_scale = Vector3{ document["Scale"][0].GetFloat(), document["Scale"][1].GetFloat(), 1 };
-//	}
-//
-//	if (document.HasMember("Rotate"))	//Checks if the variable exists in .Json file
-//	{
-//		_rotationAngle = (document["Rotate"].GetFloat());
-//	}
-//}
-
-TransformComponent::TransformComponent(GameObject* parent, size_t uId, IComponent* component)
-	:IComponent(parent, uId),
-	_pos{ Vector3{ 0, 0, 1} },
+TransformComponent::TransformComponent()
+	:_pos{ Vector3{ 0, 0, 1 } },
 	_scale{ Vector3{ 100, 100, 1 } },
-	_rotationAngle{ 0.0f }
+	_rotationAngle{ 0.0f },
+	_pivotPoint{ Vector3{ 0, 0, 1 } }
 {
-	if (component)
-	{
-		TransformComponent* transformComponent = dynamic_cast<TransformComponent*>(component);
-		_pos = transformComponent->_pos;
-		_scale = transformComponent->_scale;
-		_rotationAngle = transformComponent->_rotationAngle;
-	}
 }
+
+TransformComponent::TransformComponent(const Vector3& pos, const Vector3& scale, const float& angle) :
+	_pos{ pos }, _scale{ scale }, _rotationAngle{ angle }, _pivotPoint{ pos }
+{
+
+}
+
 
 
 
@@ -286,4 +268,185 @@ void TransformComponent::Init()
 
 	SetRotate(_localRotationAngle);
 	_init = true;
+}
+
+void TransformComponent::SerialiseComponent(Serialiser& document)
+{
+	if (document.HasMember("Position") && document["Position"].IsArray())	//Checks if the variable exists in .Json file
+	{
+		if (document["Position"][0].IsFloat() && document["Position"][1].IsFloat())	//Check the array values
+			_localPos = Vector3{ document["Position"][0].GetFloat(), document["Position"][1].GetFloat(), 1 };
+
+		if (document["Position"].Size() == 3)
+		{
+			_localPos.SetZ(document["Position"][2].GetFloat());
+		}
+	}
+
+	if (document.HasMember("Scale") && document["Scale"].IsArray())
+	{
+		if (document["Scale"][0].IsFloat() && document["Scale"][1].IsFloat())	//Check the array values
+			_scale = Vector3{ document["Scale"][0].GetFloat(), document["Scale"][1].GetFloat(), 1 };
+
+		if (document["Scale"].Size() == 3)
+		{
+			_scale.SetZ(document["Scale"][2].GetFloat());
+		}
+	}
+
+	if (document.HasMember("Rotate") && document["Rotate"].IsFloat())	//Checks if the variable exists in .Json file
+	{
+		_localRotationAngle = (document["Rotate"].GetFloat());
+	}
+
+	if (document.HasMember("PivotPosition") && document["PivotPosition"].IsArray())	//Checks if the variable exists in .Json file
+	{
+		if (document["PivotPosition"][0].IsFloat() && document["PivotPosition"][1].IsFloat())	//Check the array values
+			_localPivotPoint = Vector3{ document["PivotPosition"][0].GetFloat(), document["PivotPosition"][1].GetFloat(), 1 };
+
+		if (document["PivotPosition"].Size() == 3)
+		{
+			_localPivotPoint.SetZ(document["PivotPosition"][2].GetFloat());
+		}
+	}
+}
+
+void TransformComponent::DeSerialiseComponent(DeSerialiser& prototypeDoc)
+{
+	rapidjson::Value value;
+
+	value.SetBool(true);
+	prototypeDoc.AddMember("TransformComponent", rapidjson::Value(true));
+
+	value.SetArray();
+	value.PushBack(rapidjson::Value(_pos.GetX()).Move(), prototypeDoc.Allocator());
+	value.PushBack(rapidjson::Value(_pos.GetY()).Move(), prototypeDoc.Allocator());
+	value.PushBack(rapidjson::Value(_pos.GetZ()).Move(), prototypeDoc.Allocator());
+	prototypeDoc.AddMember("Position", value);
+
+	value.SetArray();
+	value.PushBack(rapidjson::Value(_scale.GetX()).Move(), prototypeDoc.Allocator());
+	value.PushBack(rapidjson::Value(_scale.GetY()).Move(), prototypeDoc.Allocator());
+	value.PushBack(rapidjson::Value(_scale.GetZ()).Move(), prototypeDoc.Allocator());
+	prototypeDoc.AddMember("Scale", value);
+
+	value.SetFloat(_rotationAngle);
+	prototypeDoc.AddMember("Rotate", value);
+
+	_pivotPoint -= _pos;
+	value.SetArray();
+	value.PushBack(rapidjson::Value(_pivotPoint.GetX()).Move(), prototypeDoc.Allocator());
+	value.PushBack(rapidjson::Value(_pivotPoint.GetY()).Move(), prototypeDoc.Allocator());
+	value.PushBack(rapidjson::Value(_pivotPoint.GetZ()).Move(), prototypeDoc.Allocator());
+	prototypeDoc.AddMember("PivotPosition", value);
+	_pivotPoint += _pos;
+}
+
+void TransformComponent::DeSerialiseComponent(rapidjson::Value& prototypeDoc, rapidjson::MemoryPoolAllocator<>& allocator)
+{
+	rapidjson::Value value;
+
+	value.SetBool(true);
+	prototypeDoc.AddMember("TransformComponent", rapidjson::Value(true), allocator);
+
+	value.SetArray();
+	value.PushBack(rapidjson::Value(_pos.GetX()).Move(), allocator);
+	value.PushBack(rapidjson::Value(_pos.GetY()).Move(), allocator);
+	value.PushBack(rapidjson::Value(_pos.GetZ()).Move(), allocator);
+	prototypeDoc.AddMember("Position", value, allocator);
+
+	value.SetArray();
+	value.PushBack(rapidjson::Value(_scale.GetX()).Move(), allocator);
+	value.PushBack(rapidjson::Value(_scale.GetY()).Move(), allocator);
+	value.PushBack(rapidjson::Value(_scale.GetZ()).Move(), allocator);
+	prototypeDoc.AddMember("Scale", value, allocator);
+
+	value.SetFloat(_rotationAngle);
+	prototypeDoc.AddMember("Rotate", value, allocator);
+
+	_pivotPoint -= _pos;
+	value.SetArray();
+	value.PushBack(rapidjson::Value(_pivotPoint.GetX()).Move(), allocator);
+	value.PushBack(rapidjson::Value(_pivotPoint.GetY()).Move(), allocator);
+	value.PushBack(rapidjson::Value(_pivotPoint.GetZ()).Move(), allocator);
+	prototypeDoc.AddMember("PivotPosition", value, allocator);
+	_pivotPoint += _pos;
+}
+
+void TransformComponent::DeserialiseComponentSceneFile(IComponent* protoCom, rapidjson::Value& value, rapidjson::MemoryPoolAllocator<>& allocator)
+{
+	TransformComponent* protoTransformCom = dynamic_cast<TransformComponent*>(protoCom);
+
+	if (!protoTransformCom)
+	{
+		DeSerialiseComponent(value, allocator);
+		return;
+	}
+
+	bool addComponentIntoSceneFile = false;
+	rapidjson::Value position;
+	rapidjson::Value scale;
+	rapidjson::Value rotate;
+
+	rapidjson::Value pivot;
+
+	if (protoTransformCom->_pos != _pos)
+	{
+		position.SetArray();
+		addComponentIntoSceneFile = true;
+		position.PushBack(rapidjson::Value(_pos._x), allocator);
+		position.PushBack(rapidjson::Value(_pos._y), allocator);
+		position.PushBack(rapidjson::Value(_pos._z), allocator);
+	}
+
+	if (protoTransformCom->_scale != _scale)
+	{
+		scale.SetArray();
+		addComponentIntoSceneFile = true;
+		scale.PushBack(rapidjson::Value(_scale._x), allocator);
+		scale.PushBack(rapidjson::Value(_scale._y), allocator);
+		scale.PushBack(rapidjson::Value(_scale._z), allocator);
+	}
+
+	if (protoTransformCom->_rotationAngle != _rotationAngle)
+	{
+		addComponentIntoSceneFile = true;
+		rotate.SetFloat(_rotationAngle);
+	}
+
+	if (protoTransformCom->_pivotPoint != _pivotPoint)
+	{
+		_pivotPoint -= _pos;
+		pivot.SetArray();
+		addComponentIntoSceneFile = true;
+		pivot.PushBack(rapidjson::Value(_pivotPoint._x), allocator);
+		pivot.PushBack(rapidjson::Value(_pivotPoint._y), allocator);
+		pivot.PushBack(rapidjson::Value(_pivotPoint._z), allocator);
+		_pivotPoint += _pos;
+	}
+
+	if (addComponentIntoSceneFile)	//If anyone of component data of obj is different from Prototype
+	{
+		value.AddMember("TransformComponent", rapidjson::Value(true), allocator);
+
+		if (!position.IsNull())	//if rapidjson::value container is not empty
+		{
+			value.AddMember("Position", position, allocator);
+		}
+
+		if (!scale.IsNull())
+		{
+			value.AddMember("Scale", scale, allocator);
+		}
+
+		if (!rotate.IsNull())
+		{
+			value.AddMember("Rotate", rotate, allocator);
+		}
+
+		if (!pivot.IsNull())
+		{
+			value.AddMember("PivotPosition", pivot, allocator);
+		}
+	}
 }
--- MiracleEngine/Src/Engine/GameObject/Components/IdentityComponent.cpp
+++ MiracleEngine/Src/Engine/GameObject/Components/IdentityComponent.cpp
@@ -33,6 +33,27 @@ void IdentityComponent::Init()
 		MyFactory.SaveNewLinkID(_linkUId, GetParentId());
 }
 
+
+const std::string& IdentityComponent::GetName() const
+{
+	return _name;
+}
+
+const std::string& IdentityComponent::ObjectType() const
+{
+	return _ObjectType;
+}
+
+void IdentityComponent::SetObjectType(std::string ObjectType)
+{
+	_ObjectType = ObjectType;
+}
+
+int IdentityComponent::GetLinkID()
+{
+	return _linkUId;
+}
+
 void IdentityComponent::Inspect()
 {
 	ImGui::Spacing();
@@ -68,3 +89,102 @@ void IdentityComponent::Inspect()
 //{
 //	return _typeId;
 //}
+
+void IdentityComponent::SerialiseComponent(Serialiser& document)
+{
+	if (document.HasMember("Name") && document["Name"].IsString())	//Checks if the variable exists in .Json file
+	{
+		_name = document["Name"].GetString();
+	}
+
+	//if (document.HasMember("GameObjectType") && document["GameObjectType"].IsInt())	//Checks if the variable exists in .Json file
+	//{
+	//	_typeId = document["GameObjectType"].GetInt();
+	//}	
+
+	if (document.HasMember("ObjectType") && document["ObjectType"].IsString())	//Checks if the variable exists in .Json file
+	{
+		_ObjectType = document["ObjectType"].GetString();
+	}
+
+	if (document.HasMember("LinkUId") && document["LinkUId"].IsInt())
+	{
+		_linkUId = document["LinkUId"].GetInt();
+	}
+}
+
+void IdentityComponent::DeSerialiseComponent(DeSerialiser& prototypeDoc)
+{
+	rapidjson::Value value;
+
+	value.SetBool(true);
+	prototypeDoc.AddMember("IdentityComponent", rapidjson::Value(true));
+
+	value.SetString(rapidjson::StringRef(_ObjectType.c_str()));
+	prototypeDoc.AddMember("ObjectType", value);
+
+	value.SetString(rapidjson::StringRef(_name.c_str()));
+	prototypeDoc.AddMember("Name", value);
+
+	value.SetInt(_linkUId);
+	prototypeDoc.AddMember("LinkUId", value);
+
+	//value.SetInt(_typeId);
+	//prototypeDoc.AddMember("GameObjectType", value);
+}
+
+void IdentityComponent::DeSerialiseComponent(rapidjson::Value& prototypeDoc, rapidjson::MemoryPoolAllocator<>& allocator)
+{
+	rapidjson::Value value;
+
+	value.SetBool(true);
+	prototypeDoc.AddMember("IdentityComponent", rapidjson::Value(true), allocator);
+
+	value.SetString(rapidjson::StringRef(_ObjectType.c_str()));
+	prototypeDoc.AddMember("ObjectType", value, allocator);
+
+	value.SetString(rapidjson::StringRef(_name.c_str()));
+	prototypeDoc.AddMember("Name", value, allocator);
+
+	value.SetInt(_linkUId);
+	prototypeDoc.AddMember("LinkUId", value, allocator);
+
+	//value.SetInt(_typeId);
+	//prototypeDoc.AddMember("GameObjectType", value);
+}
+
+void IdentityComponent::DeserialiseComponentSceneFile(IComponent* protoCom, rapidjson::Value& value, rapidjson::MemoryPoolAllocator<>& allocator)
+{
+
+	IdentityComponent* protoIdentityCom = dynamic_cast<IdentityComponent*>(protoCom);
+
+	bool addComponentIntoSceneFile = false;
+
+	rapidjson::Value ObjectType;
+	rapidjson::Value LinkUId;
+
+	ObjectType.SetString(rapidjson::StringRef(_ObjectType.c_str()));
+
+	rapidjson::Value Name("IdentityComponent", allocator);
+	value.AddMember(Name, rapidjson::Value(true), allocator);
+	Name.SetString("ObjectType", allocator);
+	value.AddMember(Name, ObjectType, allocator);
+
+	rapidjson::Value name;
+	if (protoIdentityCom->_name.compare(_name))	//If audiofile of Object is diff from prototype
+	{
+		addComponentIntoSceneFile = true;
+		name.SetString(rapidjson::StringRef(_name.c_str()));
+
+		Name.SetString("Name", allocator);
+		value.AddMember(Name, name, allocator);
+	}
+
+	if (protoIdentityCom->_linkUId != _linkUId)
+	{
+		addComponentIntoSceneFile = true;
+		LinkUId.SetInt(_linkUId);
+
+		value.AddMember("LinkUId", _linkUId, allocator);
+	}
+}
