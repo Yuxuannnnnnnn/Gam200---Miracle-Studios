4c56afc64598c4604d41cac75cb9da5c25f3e227
bnjj292 <bnjj292@users.noreply.github.com>
2020-03-04 21:18:23 +0800

    Update
    Added Boss()
    player.json add AudioComp

--- MiracleEngine/Src/Script/Boss.cpp
+++ MiracleEngine/Src/Script/Boss.cpp
@@ -1,38 +1,136 @@
 #include "PrecompiledHeaders.h"
 #include "Script/Boss.h"
+#include <cstdlib>
+#include <ctime>
+#include "EntrancePortal.h"
 
-//Boss::Boss()
-//{
-//
-//}
-//Boss* Boss::Clone()
-//{
-//	return new Boss(*this);
-//}
-//void Boss::Init()
-//{
-//
-//}
-//void Boss::Update(double dt)
-//{
-//
-//}
-//
-//Vector3& Boss::GetDestinationPos()
-//{
-//
-//}
-//Vector3& Boss::GetPosition()
-//{
-//
-//}
-//
-//void Boss::FSM()
-//{
-//
-//}
-//
-//void Boss::OnCollision2DTrigger(Collider2D* other)
-//{
-//
-//}
+Boss::Boss() :
+	health{ 0 }, healthMax{ 0 }, healthHalf{ 0 }, healthQuart{ 0 },
+
+	idleTimer{ 0.0 }, idleDuration{ 0.0 },
+	bulletFireAmount{ 0 }, bulletFireAmountMax{ 0 }, bulletROF{ 0.0 }, rotationspeed{ 0.0f },
+
+	laserChargeTimer{ 0.0 }, laserChargeDuration{ 0.0 },
+	laserFlashTimer{ 0.0 }, laserFlashDuration{ 0.0 },
+	laserAliveTimer{ 0.0 }, laserAliveDuration{ 0.0 },
+
+	laserRapidFireAmount{ 0 }, laserRelatedDurationSpeedUp{ 0.0 },
+
+	_init{ false }, _deathStart{ false },
+	_state{ Boss_State::IDLE }, _statePrev{ Boss_State::IDLE },
+	playerId{ 0 }, playerPtr{ nullptr }, _dt{ 0.0 }
+{
+}
+
+Boss* Boss::Clone()
+{
+	return new Boss(*this);
+}
+
+void Boss::Init()
+{
+	// find player
+	for (auto itr : _engineSystems._factory->getObjectlist())
+	{
+		if ((((IdentityComponent*)itr.second->GetComponent(ComponentId::CT_Identity))->ObjectType().compare("Player01") == 0 ||
+			((IdentityComponent*)itr.second->GetComponent(ComponentId::CT_Identity))->ObjectType().compare("Player") == 0 ||
+			((IdentityComponent*)itr.second->GetComponent(ComponentId::CT_Identity))->ObjectType().compare("player") == 0) &&
+			(((LogicComponent*)itr.second->GetComponent(ComponentId::CT_Logic))->GetScript2Id(ScriptType::SCRIPT_Player)))
+		{
+			playerPtr = itr.second;
+			playerId = itr.second->Get_uID();
+			break;
+		}
+	}
+	_init = true;
+}
+void Boss::Update(double dt)
+{
+	if (_init)
+		Init();
+	_dt = dt;
+	UpdateState();
+	RunState();
+}
+
+void Boss::UpdateState()
+{
+	// states that will force skip UpdateState()
+	if (_state == Boss_State::DEATH)
+		return;
+	if (_state == Boss_State::IDLE)
+		return;
+
+	// check death
+	if (health < 0)
+	{
+		_state = Boss_State::DEATH;
+		return;
+	}
+	// determine which state Boss is in
+	if (health < healthHalf)
+		HalfAtk();
+	else if (health < healthQuart)
+		QuartAtk();
+	else
+	{
+		// default shooting
+		// spin and shoot
+	}
+}
+
+void Boss::RunState()
+{
+	if (_state == _statePrev)
+		return;
+	switch (_state)
+	{
+	case Boss_State::IDLE:
+		Idle();
+		break;
+	case Boss_State::DEATH:
+		Death();
+		break;
+	case Boss_State::CHARGING_LASER:
+		break;
+	default:
+		break;
+	}
+}
+
+
+void Boss::Idle()
+{
+	idleTimer -= _dt;
+	if (idleTimer < 0.0)
+	{
+		// reset timer
+		idleTimer = idleDuration;
+		// set to Idle_End
+		_state = Boss_State::IDLE_END;
+	}
+}
+
+void Boss::Death()
+{
+	
+}
+
+void Boss::SpinAround()
+{
+	// rotate = rotatespd * dt
+}
+
+void Boss::LookAtPlayer()
+{
+	GameObject* exist = MyFactory.GetObjectWithId(playerId);
+	if (exist != nullptr)
+	{
+		Vector3 dirVec = ((TransformComponent*)exist->GetComponent(ComponentId::CT_Transform))->GetPositionA() -
+			((TransformComponent*)this->GetSibilingComponent(ComponentId::CT_Transform))->GetPositionA();
+		Vector3 compareVec = { 0, 1, 0 };
+		float dot = dirVec._x * compareVec._x + dirVec._y * compareVec._y;
+		float det = dirVec._x * compareVec._y - dirVec._y * compareVec._x;
+		((TransformComponent*)(GetSibilingComponent(ComponentId::CT_Transform)))->GetRotate() = -atan2(det, dot);
+	}
+}
--- MiracleEngine/Src/Script/Boss.h
+++ MiracleEngine/Src/Script/Boss.h
@@ -4,127 +4,97 @@
 #ifndef BOSS_H
 #define	BOSS_H
 
-/*
-	check what values needed for serialisation
-		DONT do serialisation, wait for YX to fin, then follow the standard
-	check animations & sounds, need follow animation methods in Enemy.h.cpp
-	check if need ParentChild.
+enum class Boss_State {
+	IDLE = 0,
+	IDLE_END,
+	DEATH,
+	SPIN_SHOOTBULLET,
+	CHARGING_LASER,
+	FIRING_LASER,
+	RAPID_CHARGING_LASER,
+	RAPID_FIRING_LASER,
+};
 
+class Boss : public IScript2
+{
 private:
-
-// vals that need serialisation
 	int health, healthMax, healthHalf, healthQuart; // Half&Quart dont need serial
-	double bulletROF, laserCharge, laserShootDuration;
-	double attackDuration, attack related timings;
-	float rotation speed;
+	double idleTimer, idleDuration; // after every attack, rest for this long
+
+	int bulletFireAmount, bulletFireAmountMax;
+	double bulletROF; // shoot bullet related stuff
+	float rotationspeed;
+
+	double laserChargeTimer, laserChargeDuration; // laser charge
+	double laserFlashTimer, laserFlashDuration; // laser short flash
+	double laserAliveTimer, laserAliveDuration; // laser lifetime, the actual laser that does dmg
+
+	int laserRapidFireAmount; // how many shots
+	double laserRelatedDurationSpeedUp; // multiplier to the durations above, 2 will make laser shoot 2x faster, 3 gives 3x, etc
+
 // non-serail vals
-	bool _init;
-	int rand;
-	GameObject* Player;
+	bool _init, _deathStart;
+	double _timerDeath;
+	Boss_State _state, _statePrev;
+	int playerId;
+	GameObject* playerPtr;
+	double _dt;
 
 public:
-	
+	Boss();
+	Boss* Clone();
+	void Init();
+	void Update(double dt);
+// Logic - Behaviour
+	void UpdateState();
+	void RunState();
 
-	void Init()
-	{
-		// find player
-		// set the max, half, quart HP vals
-		_init = true;
-	}
-	void UPDATE()
-	{
-		if (_init)
-			Init();
-
-		if(Health is less than 50%)
-			HalfAtk();
-		else if(Health is less than 25%)
-			QuartAtk();
-		else
-		{
-			// default shooting
-			// spin and shoot
-		}
-	}
-
-
-	LookAtPlayer()
-	SpinAround()
-	ShootBullet() // should just be shoot in direction Boss is facing
-	ShootLaser() // will need multiple phases within ShootLaser
+	void Idle();
+	void Death();
+	void SpinAround();
+	void LookAtPlayer();
+	void ShootBullet() {}// should just be shoot in direction Boss is facing
+	void ShootLaser() {} // will need multiple phases within ShootLaser
 		// look at player whie charging
 		// once charged flash the line of laser, then shoot laser
 		// double chargeTime, flashTime, 
-	RandNormalAtk() {
-		// call random function to see which Attack to use
-	}
-	FirstAttack() {
-		for(int i = 0; i < 25; i++)
+	void SpinShoot() {
+		// rotate clockwise
+		//shoot enemy
+		for (int i = 0; i < 25; i++)
 			ShootBullet();
 	}
-	SecondAttack() {
-		SpinAround()
-		for(int i = 0; i < 25; i++)
-			ShootBullet();
+	void SecondAttack() {
+		SpinAround();
+			for (int i = 0; i < 25; i++)
+				ShootBullet();
 	}
-	void FirstSpecialAttack() {				// whats this?
-		check number of attacks made;
-		if(number of attacks exceed 4)
-			use special attack;
+	void FirstSpecialAttack() {
+		// rapid fire laser
 	}
-	HalfAtk() { ShootLaser() }
-	QuartAtk() { FirstSpecialAttack () }
+	void HalfAtk() { ShootLaser(); }
+	void QuartAtk() { FirstSpecialAttack(); }
 
 
+	void OnCollision2DTrigger(Collider2D* other)
+	{}
 
-*/
 
+	void SerialiseComponent(Serialiser& document)
+	{
+	}
+	void DeSerialiseComponent(DeSerialiser& prototypeDoc)
+	{
+	}
+	virtual void DeSerialiseComponent(rapidjson::Value& prototypeDoc, rapidjson::MemoryPoolAllocator<>& allocator)
+	{
+	}
 
-//enum class BossStates {
-//	DEAD = 0,
-//
-//};
-//
-//class Boss : public IScript2
-//{
-//private:
-//// Logic Data - General
-//	bool _init;
-//	int _health, _healthMax, _healthRage;
-//	double _timerAttackNormal, _frequencyAttackNormal;
-//	double _timerAttackRage, _frequencyAttackRage;
-//// Logic - Behaviour
-//	GameObject* _target;
-//	int _state;
-//
-//public:
-//	void SerialiseComponent(Serialiser& document)
-//	{
-//
-//	}
-//	void DeSerialiseComponent(DeSerialiser& prototypeDoc)
-//	{
-//
-//	}
-//	void Inspect()
-//	{
-//
-//	}
-//	void DeserialiseComponentSceneFile(IComponent* protoCom, rapidjson::Value& value, rapidjson::MemoryPoolAllocator<>& allocator)
-//	{
-//	}
-//
-//	Boss();
-//	Boss* Clone();
-//	void Init();
-//	void Update(double dt);
-//
-//	Vector3& GetDestinationPos();	// gets _target's position
-//	Vector3& GetPosition();	// gets _parent's position
-//
-//	void FSM();	
-//
-//	void OnCollision2DTrigger(Collider2D* other);
-//};
-
+	virtual void DeserialiseComponentSceneFile(IComponent* protoCom, rapidjson::Value& value, rapidjson::MemoryPoolAllocator<>& allocator)
+	{
+	}
+	void Inspect()
+	{
+	}
+};
 #endif
