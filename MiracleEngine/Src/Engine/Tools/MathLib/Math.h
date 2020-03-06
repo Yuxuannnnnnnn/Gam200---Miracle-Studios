e11a33aecdbf757267976e13bd87b5f509b17afd
yinshuyu <love0love.ys@gmail.com>
2020-03-06 22:49:22 +0800

    static box response

--- MiracleEngine/Src/Engine/Tools/MathLib/Math.h
+++ MiracleEngine/Src/Engine/Tools/MathLib/Math.h
@@ -13,12 +13,14 @@
 #include "Vector3.h"
 #include "Matrix3x3.h"
 #include "Matrix4x4.h"
+#include <limits>
 
 #define MY_PI 3.141592653589793238463
 
 using namespace mathLib;
 
 float Vec3Distance_LinetoPoint(Vector3 P, Vector3 Q, Vector3 R);
+bool Vec3D_InLineRange(Vector3 P, Vector3 Q, Vector3 R);
 
 float RadToDeg(float& rad);
 float DegToRad(float& deg);
